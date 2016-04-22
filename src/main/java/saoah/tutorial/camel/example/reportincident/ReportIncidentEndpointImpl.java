5bb5816fb2e8d22209e7d99e898a8db1900189dd
Salah - A608978 <salahoukoud@gmail.com>
2016-04-22 11:09:59 +0100

    # Add Camel dependency # Add Camel to code

--- src/main/java/saoah/tutorial/camel/example/reportincident/ReportIncidentEndpointImpl.java
+++ src/main/java/saoah/tutorial/camel/example/reportincident/ReportIncidentEndpointImpl.java
@@ -1,9 +1,26 @@
 package saoah.tutorial.camel.example.reportincident;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.component.log.LogComponent;
+import org.apache.camel.impl.DefaultCamelContext;
+
 /**
  * The webservice we have implemented.
  */
 public class ReportIncidentEndpointImpl implements ReportIncidentEndpoint {
+
+	private CamelContext camel;
+
+	public ReportIncidentEndpointImpl() throws Exception {
+		// create the camel context that is the "heart" of Camel
+		camel = new DefaultCamelContext();
+
+		// add the log component
+		camel.addComponent("log", new LogComponent());
+
+		// start Camel
+		camel.start();
+	}
 
 	public OutputReportIncident reportIncident(InputReportIncident parameters) {
 		System.out.println("Hello ReportIncidentEndpointImpl is called from " + parameters.getGivenName());
