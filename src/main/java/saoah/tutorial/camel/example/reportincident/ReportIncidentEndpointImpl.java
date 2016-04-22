b6e1efbbed4aaeb66390d61acc22971293a76e9e
Salah - A608978 <salahoukoud@gmail.com>
2016-04-22 11:48:48 +0100

    # Logging with Camel

--- src/main/java/saoah/tutorial/camel/example/reportincident/ReportIncidentEndpointImpl.java
+++ src/main/java/saoah/tutorial/camel/example/reportincident/ReportIncidentEndpointImpl.java
@@ -1,6 +1,10 @@
 package saoah.tutorial.camel.example.reportincident;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.Component;
+import org.apache.camel.Endpoint;
+import org.apache.camel.Exchange;
+import org.apache.camel.Producer;
 import org.apache.camel.component.log.LogComponent;
 import org.apache.camel.impl.DefaultCamelContext;
 
@@ -23,10 +27,47 @@ public class ReportIncidentEndpointImpl implements ReportIncidentEndpoint {
 	}
 
 	public OutputReportIncident reportIncident(InputReportIncident parameters) {
-		System.out.println("Hello ReportIncidentEndpointImpl is called from " + parameters.getGivenName());
+		String name = parameters.getGivenName() + " " + parameters.getFamilyName();
+
+		// let Camel do something with the name
+		sendToCamelLog(name);
 
 		OutputReportIncident out = new OutputReportIncident();
 		out.setCode("OK");
 		return out;
+	}
+
+	private void sendToCamelLog(String name) {
+		try {
+			// get the log component
+			Component component = camel.getComponent("log");
+
+			// create an endpoint and configure it.
+			// Notice the URI parameters this is a common pratice in Camel to configure
+			// endpoints based on URI.
+			// com.mycompany.part2 = the log category used. Will log at INFO level as default
+			Endpoint endpoint = component.createEndpoint("log:saoah.tutorial.camel");
+
+			// create an Exchange that we want to send to the endpoint
+			Exchange exchange = endpoint.createExchange();
+			// set the in message payload (=body) with the name parameter
+			exchange.getIn().setBody(name);
+
+			// now we want to send the exchange to this endpoint and we then need a producer
+			// for this, so we create and start the producer.
+			Producer producer = endpoint.createProducer();
+			producer.start();
+			// process the exchange will send the exchange to the log component, that will process
+			// the exchange and yes log the payload
+			producer.process(exchange);
+
+			// stop the producer, we want to be nice and cleanup
+			producer.stop();
+
+		} catch (Exception e) {
+			// we ignore any exceptions and just rethrow as runtime
+			throw new RuntimeException(e);
+
+		}
 	}
 }
